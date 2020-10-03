d2a54ebb9d4265bb3d3ae58c175a9bbc0bda111b
925577835 <925577835@qq.com>
2020-10-03 18:33:21 +0800

    feat: test router

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,58 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <AppMenu></AppMenu>
+    <div id="app">
+        <div id="nav">
+            <AppMenu></AppMenu>
+        </div>
+        <div id="body">
+            <router-view class="view" />
+        </div>
     </div>
-    <div id="body">
-      <router-view class="view" />
-    </div>
-  </div>
 </template>
 
 <script>
 import AppMenu from '@/components/menu/AppMenu.vue'
 
 export default {
-  components: {
-    AppMenu
-  }
+    components: {
+        AppMenu
+    }
 }
 </script>
 
 <style lang="scss">
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    height: 1080px;
-    display: flex;
-    flex-direction: column;
-  }
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        height: 1080px;
+        display: flex;
+        flex-direction: column;
+    }
 
-  #nav {
-    padding: 30px;
-  }
+    #nav {
+        padding: 30px;
+    }
 
-  #body {
-    flex: 1;
-    box-sizing: border-box;
-    overflow: hidden;
-    padding: 0;
-    position: relative;
-  }
+    #body {
+        flex: 1;
+        box-sizing: border-box;
+        overflow: hidden;
+        padding: 0;
+        position: relative;
+    }
 
-  .view {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-  }
+    .view {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
 
-  html, body {
-    margin: 0
-  }
+    html,
+    body {
+        margin: 0;
+    }
 </style>
--- src/components/menu/MenuData.js
+++ src/components/menu/MenuData.js
@@ -1,20 +1,20 @@
 import { routes } from '@/router/index.js'
 
 function recursive (route) {
-  const obj = {
-    path: route.path.includes('/') ? route.path : `/${route.path}`,
-    title: route.name.toUpperCase(),
-    icon: 'el-icon-eleme'
-  }
-  const children = route.children ? route.children.reduce((acc, cur) => {
-    acc.push(recursive(cur))
-    return acc
-  }, []) : null
-  children && (obj.children = children)
-  return obj
+    const obj = {
+        path: route.path.includes('/') ? route.path : `/${route.path}`,
+        title: route.name.toUpperCase(),
+        icon: 'el-icon-eleme'
+    }
+    const children = route.children ? route.children.reduce((acc, cur) => {
+        acc.push(recursive(cur))
+        return acc
+    }, []) : null
+    children && (obj.children = children)
+    return obj
 }
 
 export default routes.reduce((acc, cur) => {
-  acc.push(recursive(cur))
-  return acc
+    acc.push(recursive(cur))
+    return acc
 }, [])
